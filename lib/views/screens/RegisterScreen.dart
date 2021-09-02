9b3268c64e4ce95d1491e238f26bbb5f763a1c91
Md Nife Salil Islam <nifesalilislam@gmail.com>
2021-09-02 18:53:31 +0530

    Update RegisterScreen.dart

--- lib/views/screens/RegisterScreen.dart
+++ lib/views/screens/RegisterScreen.dart
@@ -8,11 +8,350 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController emailFieldController = new TextEditingController();
+  TextEditingController nameFieldController = new TextEditingController();
+  TextEditingController mobileNumberFieldController =
+      new TextEditingController();
+  TextEditingController passwordFieldController = new TextEditingController();
+  TextEditingController birthdayFieldController = new TextEditingController();
+
+  FocusNode emailFocusNode = new FocusNode();
+  FocusNode nameFocusNode = new FocusNode();
+  FocusNode mobileNumberFocusNode = new FocusNode();
+  FocusNode passwordFocusNode = new FocusNode();
+  FocusNode birthdayFocusNode = new FocusNode();
+
+  late DateTime _selectedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _selectedDate = DateTime.now();
+      birthdayFieldController.text = _selectedDate.toString();
+    });
+  }
+
+  //Method for showing the date picker
+  void _pickDateDialog() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            //which date will display when user open the picker
+            firstDate: DateTime(1950),
+            //what will be the previous supported year in picker
+            lastDate: DateTime
+                .now()) //what will be the up to supported date in picker
+        .then((pickedDate) {
+      //then usually do the future job
+      if (pickedDate == null) {
+        //if user tap cancel then this function will stop
+        return;
+      }
+      setState(() {
+        //for rebuilding the ui
+        _selectedDate = pickedDate;
+        birthdayFieldController.text = _selectedDate.toString();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
-        child: Text('Hello'),
+        child: Container(
+          width: screenWidth,
+          height: screenHeight,
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  child: Image.asset("assets/logo/prestar.png"),
+                  radius: 50,
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Text(
+                    'Full Name',
+                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                  width: double.infinity,
+                ),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  controller: nameFieldController,
+                  focusNode: nameFocusNode,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person_outline),
+                    // suffixIcon: Icon(Icons.assign),
+                    hintText: 'Enter your name',
+                    hintStyle: TextStyle(color: Colors.black54, fontSize: 16),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  child: Text(
+                    'Your Email',
+                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                  width: double.infinity,
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailFieldController,
+                  focusNode: emailFocusNode,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email_outlined),
+                    hintText: 'Enter your email',
+                    hintStyle: TextStyle(color: Colors.black54, fontSize: 16),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  child: Text(
+                    'Mobile No.',
+                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                  width: double.infinity,
+                ),
+                TextField(
+                  keyboardType: TextInputType.phone,
+                  controller: mobileNumberFieldController,
+                  focusNode: mobileNumberFocusNode,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.phone_android_outlined),
+                    alignLabelWithHint: false,
+                    hintText: 'Enter your mobile number',
+                    hintStyle: TextStyle(color: Colors.black54, fontSize: 16),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  child: Text(
+                    'Password',
+                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                  width: double.infinity,
+                ),
+                TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  controller: passwordFieldController,
+                  focusNode: passwordFocusNode,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock_outline),
+                    suffixIcon: Icon(Icons.visibility),
+                    alignLabelWithHint: false,
+                    hintText: 'Choose password',
+                    hintStyle: TextStyle(color: Colors.black54, fontSize: 16),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  child: Text(
+                    'Date of Birth',
+                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                  width: double.infinity,
+                ),
+                TextField(
+                  keyboardType: TextInputType.datetime,
+                  controller: birthdayFieldController,
+                  // focusNode: birthdayFocusNode,
+                  decoration: InputDecoration(
+                    prefixIcon: IconButton(
+                      onPressed: _pickDateDialog,
+                      icon: Icon(Icons.calendar_today),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  child: Text(
+                    'Gender',
+                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                  width: double.infinity,
+                ),
+                DropdownButton(
+                  value: 'Choose you Gender',
+                  icon: const Icon(Icons.arrow_drop_down),
+                  iconSize: 24,
+                  elevation: 16,
+                  onChanged: (String? newValue) {
+                    // setState(() {
+                    //   dropdownValue = newValue!;
+                    // });
+                  },
+                  items: ['Choose you Gender', 'Male', 'Female', 'Other']
+                      .map(
+                        (item) => DropdownMenuItem(
+                          child: Container(
+                            width: screenWidth - 64,
+                            child: Row(
+                              children: [
+                                Icon(Icons.male, color: Colors.black54),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  item,
+                                  style: TextStyle(
+                                      color: Colors.black54, fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          ),
+                          value: item,
+                        ),
+                      )
+                      .toList(),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  height: 55,
+                  child: ElevatedButton(
+                    onPressed: null,
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xff092497)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Register',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16),
+                          ),
+                          Icon(
+                            Icons.exit_to_app,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  width: double.infinity,
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: null,
+                    child: Text(
+                      'Register Now',
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 55,
+                  child: ElevatedButton(
+                    onPressed: null,
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.redAccent),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ImageIcon(
+                          AssetImage("assets/icons/google.png"),
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Login with Google',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                  ),
+                  width: double.infinity,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 55,
+                  child: ElevatedButton(
+                    onPressed: null,
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.blueAccent),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ImageIcon(
+                          AssetImage("assets/icons/facebook.png"),
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Login with Facebook',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                  ),
+                  width: double.infinity,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
