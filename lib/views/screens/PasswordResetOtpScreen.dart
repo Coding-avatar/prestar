0b8250e18939832d6ca5dd2c507980be28254ec2
Md Nife Salil Islam <nifesalilislam@gmail.com>
2021-09-03 14:12:10 +0530

    Update PasswordResetOtpScreen.dart

--- lib/views/screens/PasswordResetOtpScreen.dart
+++ lib/views/screens/PasswordResetOtpScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:otp_text_field/style.dart';
 
 class PasswordResetScreen extends StatefulWidget {
   const PasswordResetScreen({Key? key}) : super(key: key);
@@ -8,8 +10,123 @@ class PasswordResetScreen extends StatefulWidget {
 }
 
 class _PasswordResetScreenState extends State<PasswordResetScreen> {
+  String userMobileNumber = '71*****892';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: screenWidth,
+          height: screenHeight,
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 70,
+              ),
+              Text(
+                'We have sent an OTP to your Mobile',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black54, fontSize: 28),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Please check your mobile number $userMobileNumber verification OTP',
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: double.infinity,
+                // color: Colors.green,
+                child: OTPTextField(
+                  length: 4,
+                  // width: screenWidth - 250,
+                  fieldWidth: 60,
+                  style: TextStyle(fontSize: 17),
+                  textFieldAlignment: MainAxisAlignment.spaceAround,
+                  fieldStyle: FieldStyle.box,
+                  onCompleted: (pin) {
+                    print("Completed: " + pin);
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                height: 55,
+                child: ElevatedButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xff092497)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Next',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        Icon(
+                          Icons.exit_to_app,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                width: double.infinity,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                onPressed: null,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Didn\'t Receive? ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16,
+                        color: Color(0xff092497),
+                      ),
+                    ),
+                    Text(
+                      'Click Here',
+                      style: TextStyle(
+                          color: Color(0xff092497),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
