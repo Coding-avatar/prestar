202ab183f9a69f348e19b272b7eeb0dd8a0e52d3
Md Nife Salil Islam <40847683+Coding-avatar@users.noreply.github.com>
2021-09-05 11:27:27 +0530

    Update HomeScreen.dart

--- lib/views/screens/HomeScreen.dart
+++ lib/views/screens/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prestar/views/screens/ProfileScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,9 +11,116 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Image.asset(
+          "assets/logo/prestar.png",
+          height: 70,
+          width: screenWidth / 3,
+        ),
+        centerTitle: true,
+        actions: [
+          Icon(Icons.device_unknown),
+          SizedBox(
+            width: 15,
+          )
+        ],
+      ),
       body: SafeArea(
-        child: Text('Hello'),
+        child: Container(
+          width: screenWidth,
+          height: screenHeight,
+          child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.search,
+                        color: Color(0xff092497),
+                        size: 30,
+                      ),
+                      Expanded(child: TextField())
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        unselectedLabelStyle: TextStyle(color: Colors.grey),
+        items: [
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/icons/home.png"),
+            ),
+            label: 'Home',
+            tooltip: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/icons/video-camera.png"),
+            ),
+            label: 'Videos',
+            tooltip: 'Videos',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/icons/video.png"),
+            ),
+            label: 'Video',
+            tooltip: 'Video',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/icons/bell.png"),
+            ),
+            label: 'Notification',
+            tooltip: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/icons/user.png"),
+            ),
+            label: 'Profile',
+            tooltip: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              break;
+            case 1:
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ProfileScreen(),),);
+              break;
+            case 2:
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ProfileScreen(),),);
+              break;
+            case 3:
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ProfileScreen(),),);
+              break;
+            case 4:
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => ProfileScreen(),
+                ),
+              );
+              break;
+          }
+        },
       ),
     );
   }
