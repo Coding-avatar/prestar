d11dc30fcdf50b1d42b96050061ec7116604aff6
Md Nife Salil Islam <40847683+Coding-avatar@users.noreply.github.com>
2021-09-05 11:27:54 +0530

    Update ResetPasswordScreen.dart

--- lib/views/screens/ResetPasswordScreen.dart
+++ lib/views/screens/ResetPasswordScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:prestar/views/screens/HomeScreen.dart';
+
 class ResetPasswordScreen extends StatefulWidget {
   const ResetPasswordScreen({Key? key}) : super(key: key);
 
@@ -10,6 +12,138 @@ class ResetPasswordScreen extends StatefulWidget {
 class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Container(
+          width: screenWidth,
+          height: screenHeight,
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'New Password',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black54, fontSize: 28),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Please create a new password',
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 55,
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.black54),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: TextFormField(
+                  cursorColor: Colors.black,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: new InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      contentPadding: EdgeInsets.only(
+                          left: 15, bottom: 11, top: 11, right: 15),
+                      hintText: "New Password"),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 55,
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.black54),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: TextFormField(
+                  cursorColor: Colors.black,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: new InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      contentPadding: EdgeInsets.only(
+                          left: 15, bottom: 11, top: 11, right: 15),
+                      hintText: "Confirm Password"),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                height: 55,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => HomeScreen(),
+                      ),
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xff092497)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Next',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        Icon(
+                          Icons.exit_to_app,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                width: double.infinity,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
