45d756f4989643567635b36966b1651593beeaf9
Md Nife Salil Islam <nifesalilislam@gmail.com>
2021-09-09 17:43:34 +0530

    Create VideoPostScreen.dart

---
+++ lib/views/screens/VideoPostScreen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:prestar/views/screens/PostScreen.dart';
+
+import 'GoLiveDescriptionScreen.dart';
+import 'HomeScreen.dart';
+import 'ProfileScreen.dart';
+
+class VideoPostScreen extends StatefulWidget {
+  const VideoPostScreen({Key? key}) : super(key: key);
+
+  @override
+  _VideoPostScreenState createState() => _VideoPostScreenState();
+}
+
+class _VideoPostScreenState extends State<VideoPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        width: screenWidth,
+        height: screenHeight,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 3,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        unselectedLabelStyle: TextStyle(color: Colors.grey),
+        items: [
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/icons/home.png"),
+            ),
+            label: 'Home',
+            tooltip: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/icons/post.png"),
+            ),
+            label: 'Post',
+            tooltip: 'Post  ',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/icons/video-camera.png"),
+            ),
+            label: 'Go Live',
+            tooltip: 'Go Live',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/icons/video.png"),
+            ),
+            label: 'Videos',
+            tooltip: 'Videos',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage("assets/icons/user.png"),
+            ),
+            label: 'Profile',
+            tooltip: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => HomeScreen(),
+                ),
+              );
+              break;
+            case 1:
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => CreatePostScreen(),
+                ),
+              );
+              break;
+            case 2:
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => GoLiveDescriptionScreen(),
+                ),
+              );
+              break;
+            case 3:
+              break;
+            case 4:
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => ProfileScreen(),
+                ),
+              );
+              break;
+          }
+        },
+      ),
+    );
+  }
+}
