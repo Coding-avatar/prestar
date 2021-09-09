0b286b85bb5029af9f2b3874cfd48f837cf3d4cb
Md Nife Salil Islam <nifesalilislam@gmail.com>
2021-09-09 17:09:08 +0530

    Create NotificationScreen.dart

---
+++ lib/views/screens/NotificationScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({Key? key}) : super(key: key);
+
+  @override
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
