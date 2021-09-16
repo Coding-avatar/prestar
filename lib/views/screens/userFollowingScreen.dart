0eade8230a204cb6b4f6ee4bf9a0cefa6695531f
Md Nife Salil Islam <nifesalilislam@gmail.com>
2021-09-16 14:31:28 +0530

    Create userFollowingScreen.dart

---
+++ lib/views/screens/userFollowingScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UserFollowingScreen extends StatefulWidget {
+  const UserFollowingScreen({Key? key}) : super(key: key);
+
+  @override
+  _UserFollowingScreenState createState() => _UserFollowingScreenState();
+}
+
+class _UserFollowingScreenState extends State<UserFollowingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Container(),
+    );
+  }
+}
