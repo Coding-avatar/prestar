a2215fc5136160ae0c64f229d348f379012f246d
Md Nife Salil Islam <nifesalilislam@gmail.com>
2021-09-16 14:31:34 +0530

    Create userPostScreen.dart

---
+++ lib/views/screens/userPostScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UserPostScreen extends StatefulWidget {
+  const UserPostScreen({Key? key}) : super(key: key);
+
+  @override
+  _UserPostScreenState createState() => _UserPostScreenState();
+}
+
+class _UserPostScreenState extends State<UserPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Container(),
+    );
+  }
+}
