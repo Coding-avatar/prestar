adcd3b0758ca95a6e2cbfed294994f1cdedb75be
Md Nife Salil Islam <40847683+Coding-avatar@users.noreply.github.com>
2021-09-16 13:32:29 +0530

    Create myProfileScreen.dart

---
+++ lib/views/screens/myProfileScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyProfileScreen extends StatefulWidget {
+  const MyProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  _MyProfileScreenState createState() => _MyProfileScreenState();
+}
+
+class _MyProfileScreenState extends State<MyProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        width: screenWidth,
+        height: screenHeight,
+        child: Column(
+          children: [Text('This is my profile')],
+        ),
+      ),
+    );
+  }
+}
