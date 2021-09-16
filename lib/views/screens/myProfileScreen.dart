485ff1f09ec78a3b75f4d672c462833f137696e7
Md Nife Salil Islam <nifesalilislam@gmail.com>
2021-09-16 19:00:00 +0530

    Update myProfileScreen.dart

--- lib/views/screens/myProfileScreen.dart
+++ lib/views/screens/myProfileScreen.dart
@@ -8,6 +8,20 @@ class MyProfileScreen extends StatefulWidget {
 }
 
 class _MyProfileScreenState extends State<MyProfileScreen> {
+  TextEditingController userNameFieldController = new TextEditingController();
+  TextEditingController userBioFieldController = new TextEditingController();
+  TextEditingController userAgeFieldController = new TextEditingController();
+  TextEditingController userLocationFieldController =
+      new TextEditingController();
+  TextEditingController userEmailFieldController = new TextEditingController();
+  TextEditingController userMobileNumberFieldController =
+      new TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    updateProfileDetails();
+  }
+
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
@@ -16,10 +30,100 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
       body: Container(
         width: screenWidth,
         height: screenHeight,
+        padding: EdgeInsets.symmetric(horizontal: 15),
         child: Column(
-          children: [Text('This is my profile')],
+          children: [
+            CircleAvatar(
+              radius: 75,
+              backgroundImage: AssetImage("assets/images/profile2.jpg"),
+            ),
+            TextField(
+              controller: userNameFieldController,
+              decoration: InputDecoration(
+                label: Text('Name'),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: null,
+                ),
+              ),
+              enabled: false,
+            ),
+            TextField(
+              controller: userBioFieldController,
+              decoration: InputDecoration(
+                label: Text('Bio'),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: null,
+                ),
+              ),
+              maxLines: 2,
+              enabled: false,
+              maxLength: 120,
+            ),
+            TextField(
+              controller: userAgeFieldController,
+              decoration: InputDecoration(
+                label: Text('Age'),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: null,
+                ),
+              ),
+              enabled: false,
+            ),
+            TextField(
+              controller: userLocationFieldController,
+              decoration: InputDecoration(
+                label: Text('Location'),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: null,
+                ),
+              ),
+              enabled: false,
+            ),
+            TextField(
+              controller: userEmailFieldController,
+              decoration: InputDecoration(
+                label: Text('Email Address'),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: null,
+                ),
+              ),
+              enabled: false,
+            ),
+            TextField(
+              controller: userMobileNumberFieldController,
+              decoration: InputDecoration(
+                label: Text('Phone No.'),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: null,
+                ),
+                // labelStyle: TextStyle(color: Colors.black54, fontSize: 12),
+              ),
+              enabled: true,
+            ),
+            // Date Of Birth
+            // Gender
+            // Interests
+          ],
         ),
       ),
     );
+  }
+
+  void updateProfileDetails() {
+    setState(() {
+      userNameFieldController.text = "Rima Dutta";
+      userBioFieldController.text =
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard";
+      userAgeFieldController.text = "27";
+      userLocationFieldController.text = "Kolkata";
+      userEmailFieldController.text = "rimadutta1234@gmail.com";
+      userMobileNumberFieldController.text = "1234567890";
+    });
   }
 }
