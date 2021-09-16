ba4f2c0d6535bbe7d54a7b29ed8d25477c95d6b5
Md Nife Salil Islam <nifesalilislam@gmail.com>
2021-09-16 14:31:23 +0530

    Create userFollowersScreen.dart

---
+++ lib/views/screens/userFollowersScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UserFollowersScreen extends StatefulWidget {
+  const UserFollowersScreen({Key? key}) : super(key: key);
+
+  @override
+  _UserFollowersScreenState createState() => _UserFollowersScreenState();
+}
+
+class _UserFollowersScreenState extends State<UserFollowersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Container(),
+    );
+  }
+}
