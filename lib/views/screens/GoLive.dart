99e16e4ce1a13dbd25f2e7022b3414e3cb26e1dc
Md Nife Salil Islam <40847683+Coding-avatar@users.noreply.github.com>
2021-09-16 10:40:01 +0530

    Create GoLive.dart

---
+++ lib/views/screens/GoLive.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GoLiveScreen extends StatefulWidget {
+  const GoLiveScreen({Key? key}) : super(key: key);
+
+  @override
+  _GoLiveScreenState createState() => _GoLiveScreenState();
+}
+
+class _GoLiveScreenState extends State<GoLiveScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
