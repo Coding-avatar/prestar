0c4e002f5b81aa607436f0a03212a0d0af8434b8
Md Nife Salil Islam <40847683+Coding-avatar@users.noreply.github.com>
2021-09-18 10:10:50 +0530

    Update myProfileScreen.dart

--- lib/views/screens/myProfileScreen.dart
+++ lib/views/screens/myProfileScreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:multi_select_flutter/chip_display/multi_select_chip_display.dart';
+import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
+import 'package:multi_select_flutter/util/multi_select_item.dart';
+import 'package:multi_select_flutter/util/multi_select_list_type.dart';
 
 class MyProfileScreen extends StatefulWidget {
   const MyProfileScreen({Key? key}) : super(key: key);
@@ -10,12 +14,13 @@ class MyProfileScreen extends StatefulWidget {
 class _MyProfileScreenState extends State<MyProfileScreen> {
   TextEditingController userNameFieldController = new TextEditingController();
   TextEditingController userBioFieldController = new TextEditingController();
-  TextEditingController userAgeFieldController = new TextEditingController();
+  TextEditingController userDobFieldController = new TextEditingController();
   TextEditingController userLocationFieldController =
       new TextEditingController();
-  TextEditingController userEmailFieldController = new TextEditingController();
-  TextEditingController userMobileNumberFieldController =
-      new TextEditingController();
+  String dropdownValue = '';
+
+  // List<String> choices = ['Entertainment', 'Comedy', 'Action', 'one', 'tow'];
+  // List _selectedChoices = List.empty(growable: true);
   @override
   void initState() {
     super.initState();
@@ -37,40 +42,112 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
               radius: 75,
               backgroundImage: AssetImage("assets/images/profile2.jpg"),
             ),
-            TextField(
-              controller: userNameFieldController,
-              decoration: InputDecoration(
-                label: Text('Name'),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: null,
-                ),
-              ),
-              enabled: false,
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Text('Name'),
+              width: double.infinity,
             ),
-            TextField(
-              controller: userBioFieldController,
-              decoration: InputDecoration(
-                label: Text('Bio'),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: null,
-                ),
-              ),
-              maxLines: 2,
-              enabled: false,
-              maxLength: 120,
+            // TextField(
+            //   controller: userNameFieldController,
+            //   decoration: InputDecoration(
+            //     hintText: 'Your Name',
+            //     suffixIcon: IconButton(
+            //       icon: Icon(Icons.edit),
+            //       onPressed: null,
+            //     ),
+            //   ),
+            //   enabled: false,
+            // ),
+            // Container(
+            //   margin: EdgeInsets.only(top: 10),
+            //   child: Text('Gender'),
+            //   width: double.infinity,
+            // ),
+            // DropdownButton<String>(
+            //   value: dropdownValue,
+            //   icon: const Icon(Icons.arrow_downward),
+            //   iconSize: 24, isExpanded: true, isDense: false,
+            //   elevation: 16,
+            //   style: const TextStyle(color: Colors.black87),
+            //   // underline: Container(
+            //   //   height: 2,
+            //   //   color: Colors.deepPurpleAccent,
+            //   // ),
+            //   onChanged: (String? newValue) {
+            //     setState(() {
+            //       dropdownValue = newValue!;
+            //     });
+            //   },
+            //   items: <String>['Male', 'Female', 'Other']
+            //       .map<DropdownMenuItem<String>>((String value) {
+            //     return DropdownMenuItem<String>(
+            //       value: value,
+            //       child: Expanded(child: Text(value)),
+            //     );
+            //   }).toList(),
+            // ),
+            // Container(
+            //   margin: EdgeInsets.only(top: 10),
+            //   child: Text('Bio'),
+            //   width: double.infinity,
+            // ),
+            // TextField(
+            //   controller: userBioFieldController,
+            //   decoration: InputDecoration(
+            //     hintText: 'Bio',
+            //     suffixIcon: IconButton(
+            //       icon: Icon(Icons.edit),
+            //       onPressed: null,
+            //     ),
+            //   ),
+            //   maxLines: 2,
+            //   enabled: false,
+            //   maxLength: 120,
+            // ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Text('Date of Birth'),
+              width: double.infinity,
             ),
-            TextField(
-              controller: userAgeFieldController,
-              decoration: InputDecoration(
-                label: Text('Age'),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: null,
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Flexible(
+                  child: TextField(
+                    controller: userDobFieldController,
+                    decoration: InputDecoration(
+                      hintText: 'Date of Birth',
+                      hintStyle: TextStyle(),
+                    ),
+                    enabled: false,
+                  ),
                 ),
-              ),
-              enabled: false,
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () => print('hello'),
+                )
+              ],
+            ),
+            // MultiSelectDialogField(
+            //   items: choices.map((e) => MultiSelectItem(e, e)).toList(),
+            //   listType: MultiSelectListType.CHIP,
+            //   onConfirm: (values) {
+            //     _selectedChoices = values;
+            //   },
+            // ),
+            // MultiSelectChipDisplay(
+            //   items:
+            //       _selectedChoices.map((e) => MultiSelectItem(e, e)).toList(),
+            //   onTap: (value) {
+            //     setState(() {
+            //       _selectedChoices.remove(value);
+            //     });
+            //   },
+            // ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Text('Location'),
+              width: double.infinity,
             ),
             TextField(
               controller: userLocationFieldController,
@@ -83,32 +160,6 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
               ),
               enabled: false,
             ),
-            TextField(
-              controller: userEmailFieldController,
-              decoration: InputDecoration(
-                label: Text('Email Address'),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: null,
-                ),
-              ),
-              enabled: false,
-            ),
-            TextField(
-              controller: userMobileNumberFieldController,
-              decoration: InputDecoration(
-                label: Text('Phone No.'),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: null,
-                ),
-                // labelStyle: TextStyle(color: Colors.black54, fontSize: 12),
-              ),
-              enabled: true,
-            ),
-            // Date Of Birth
-            // Gender
-            // Interests
           ],
         ),
       ),
@@ -120,10 +171,9 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
       userNameFieldController.text = "Rima Dutta";
       userBioFieldController.text =
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard";
-      userAgeFieldController.text = "27";
       userLocationFieldController.text = "Kolkata";
-      userEmailFieldController.text = "rimadutta1234@gmail.com";
-      userMobileNumberFieldController.text = "1234567890";
+      userDobFieldController.text = "22/12/1997";
+      dropdownValue = 'Female';
     });
   }
 }
