924a9348497879acf3e04cc3efda4ccd2a1af894
Md Nife Salil Islam <nifesalilislam@gmail.com>
2021-09-24 15:37:00 +0530

    Create auth_provider.dart

---
+++ lib/services/auth_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:prestar/services/auth.dart';
+
+class AuthProvider extends InheritedWidget {
+  AuthProvider({required this.auth, required this.child}) : super(child: child);
+  final AuthBase auth;
+  final Widget child;
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => false;
+
+  static AuthBase of(BuildContext context) {
+    AuthProvider? provider =
+        context.dependOnInheritedWidgetOfExactType<AuthProvider>();
+    return provider!.auth;
+  }
+}
