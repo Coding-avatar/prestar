75c23a97194d9f4d0ad49a9c41574098ac78bb95
Md Nife Salil Islam <40847683+Coding-avatar@users.noreply.github.com>
2021-10-04 11:21:03 +0530

    Update HttpService.dart

--- lib/services/HttpService.dart
+++ lib/services/HttpService.dart
@@ -1,10 +1,23 @@
 import 'dart:convert';
 
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:prestar/constants/api_constants.dart';
+import 'package:prestar/models/api_models/mongoUser.dart';
 
 class HttpService {
-  Future<http.Response> registerUser({
+  Future<http.Response> findUserWithEmail({required String email}) async {
+    http.Response res =
+        await http.post(Uri.parse(ApiConstants.endpointLoginUser),
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+            },
+            body: jsonEncode({"email": email}));
+    print('Login user response: ${res.body}');
+    return res;
+  }
+
+  Future<http.Response> registerUserWithEmailAndPassword({
     required String email,
     required String name,
     bool emailIsVerified = false,
@@ -21,6 +34,45 @@ class HttpService {
               "name": name,
               "email": email,
               "phone": phone,
+            }));
+
+    print('Register User Response: ${res.body}');
+    return res;
+  }
+
+  Future<List<double>> getCurrentLocation() async {
+    var permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.whileInUse ||
+        permission == LocationPermission.always) {
+      var position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      var coordinates = [position.latitude, position.longitude];
+      return coordinates;
+    }
+    return [];
+  }
+
+  Future<http.Response> registerUserWithGoogle({
+    required String email,
+    required String name,
+    bool emailIsVerified = false,
+    bool usedEmailAuth = false,
+    bool usedGoogleAuth = false,
+  }) async {
+    http.Response res =
+        await http.post(Uri.parse(ApiConstants.endpointRegisterUser),
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+            },
+            body: jsonEncode({
+              "name": name,
+              "email": email,
+              "emailIsVerified": true,
+              "usedGoogleAuth": true,
+              "location": {
+                "type": "Point",
+                "coordinates": await getCurrentLocation()
+              }
             }));
 
     print('Register User Response: ${res.body}');
@@ -62,6 +114,17 @@ class HttpService {
     );
 
     print('Fetch user details Response: ${res.body}');
+    return res;
+  }
+
+  Future<http.Response> fetchAllPost() async {
+    http.Response res = await http.get(
+      Uri.parse(ApiConstants.fetchAllPost),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    // print('Fetch all post Response: ${res.body}');
     return res;
   }
 }
