d2e7e6b54bed0734e912f52e92b24dec896e49b8
Md Nife Salil Islam <40847683+Coding-avatar@users.noreply.github.com>
2021-10-04 11:19:58 +0530

    added new endpoints

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,4 +1,6 @@
 class ApiConstants {
-  static const Base_url = "";
+  static const Base_url = "https://om-prestar-server-alpha.herokuapp.com";
   static const endpointRegisterUser = Base_url + "/users";
+  static const endpointLoginUser = Base_url + "/users/login";
+  static const fetchAllPost = Base_url + '/posts';
 }
