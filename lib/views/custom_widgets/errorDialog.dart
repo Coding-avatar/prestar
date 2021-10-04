28fb61b218eb97d76c4b6387a1fafb15367e4351
Md Nife Salil Islam <40847683+Coding-avatar@users.noreply.github.com>
2021-10-04 11:21:22 +0530

    Update errorDialog.dart

--- lib/views/custom_widgets/errorDialog.dart
+++ lib/views/custom_widgets/errorDialog.dart
@@ -9,29 +9,85 @@ class ErrorDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return Dialog(
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
+        borderRadius: BorderRadius.circular(10),
       ),
-      elevation: 10,
-      backgroundColor: Colors.white,
+      elevation: 0,
+      backgroundColor: Colors.transparent,
       child: Container(
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(15)),
+        width: MediaQuery.of(context).size.width * .7,
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Text(titleText),
+            Text(
+              titleText,
+              style: TextStyle(color: Colors.black87, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
             SizedBox(
               height: 10,
             ),
             Icon(
               Icons.error_outline,
               color: Colors.red,
-              size: 30,
+              size: 40,
             ),
             SizedBox(
               height: 10,
             ),
-            Text(errorMessage)
+            Text(
+              errorMessage,
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                child: Text(
+                  'Close',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+// Dialog(
+// shape: RoundedRectangleBorder(
+// borderRadius: BorderRadius.circular(15),
+// ),
+// elevation: 10,
+// backgroundColor: Colors.white,
+// child: Container(
+// child: Column(
+// children: [
+// Text(titleText),
+// SizedBox(
+// height: 10,
+// ),
+// Icon(
+// Icons.error_outline,
+// color: Colors.red,
+// size: 30,
+// ),
+// SizedBox(
+// height: 10,
+// ),
+// Text(errorMessage)
+// ],
+// ),
+// ),
+// )
