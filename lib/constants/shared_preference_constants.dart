30ac1caab877b77beed7534cceae35d574b79503
Md Nife Salil Islam <nifesalilislam@gmail.com>
2021-10-04 16:01:21 +0530

    added new constants

--- lib/constants/shared_preference_constants.dart
+++ lib/constants/shared_preference_constants.dart
@@ -1,4 +1,8 @@
 class Constants {
   static const MongoDbUser = "mongouid";
+  static const MongoDbUserName = "username";
+  static const MongoDbUserEmail = "useremail";
+  static const MongoDbUserPhoto = "userphoto";
+  static const MongoDbUserBio = "userbio";
   static const FirebaseUserUid = "uid";
 }
